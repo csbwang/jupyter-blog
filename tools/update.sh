c39f7f3358ce593c5e9f329fefdcc66d57bc047a
bo.wang <bo.wang@cootek.cn>
2018-01-07 17:15:13 +0800

    20180107_17_15_11

--- tools/update.sh
+++ tools/update.sh
@@ -2,6 +2,9 @@
 commitstr=$(date +%Y%m%d_%H_%M_%S)
 cd /Users/phenix/Documents/jupyter-blog/
 make html
+git add .
+git commit -m ${commitstr}
+git push origin master
 cd /Users/phenix/Documents/jupyter-blog/output/ 
 git add .
 git commit -m ${commitstr}
